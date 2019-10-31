704692b79fa257009771eef76d29809c0fc722e2
jacadzaca <vitouejj@gmail.com>
2019-10-31 21:44:34 +0100

    restructured project, created dir for tests

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import setuptools
+
+
+setuptools.setup(
+    name='terminal chess game',
+    author='jacadzca',
+    version='0.1dev0',
+    packages=['chess'],
+    license='MIT',
+    test_suite="test",
+)
