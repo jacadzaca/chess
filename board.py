319981282bd4a7b1147fbb8b81ed2332b042a501
jacadzaca <vitouejj@gmail.com>
2019-10-31 21:34:57 +0100

    board now has tiles instead nodes, refactored make_move method

--- board.py
+++ board.py
@@ -8,38 +8,40 @@ _WHITE = 1
 
 class Board:
     def __init__(self):
-        self._nodes = []
+        self._tiles = []
         for i in range(8):
-            self._nodes.append([])
+            self._tiles.append([])
             for j in range(8):
-                self._nodes[i].append(Tile())
+                self._tiles[i].append(Tile())
         self.create_pieces()
 
     def create_pieces(self):
-        black_pawns_row = self._nodes[1]
+        black_pawns_row = self._tiles[1]
         for tile in black_pawns_row:
             tile.current_piece = Pawn(_BLACK)
-        whie_pawns_row = self._nodes[6]
+        whie_pawns_row = self._tiles[6]
         for tile in whie_pawns_row:
             tile.current_piece = Pawn(_WHITE)
 
     def get_node_at(self, positon):
-        return self._nodes[positon.y][positon.x]
+        return self._tiles[positon.y][positon.x]
 
-    def make_move(self, move_command):
-        '''move command is a string like format [digitdigit digitdigit].
+    def execute_command(self, command):
+        '''command is a string of format [digitdigit digitdigit].
         Example: "00 01" '''
-        tiles = move_command.split(' ')
+        tiles = command.split(' ')
         current_position = vector.parse_vector(tiles[0])
         desired_position = vector.parse_vector(tiles[1])
         move = current_position - desired_position
-        if self.get_node_at(current_position).current_piece.is_legal_move(move) and not self.get_node_at(desired_position).is_occupied():
+        current_tile = self.get_node_at(current_position)
+        desired_tile = self.get_node_at(desired_position)
+        if current_tile.current_piece.is_legal_move(move) and not desired_tile.is_occupied():
             pass
 
     def __str__(self):
         board_representation = '  0 1 2 3 4 5 6 7'
         row_count = -1
-        for row in self._nodes:
+        for row in self._tiles:
             row_count += 1
             for i in range(len(row)):
                 if i == 0:
