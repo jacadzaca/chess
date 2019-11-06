60ca31b4b8cb346697b35920163dcaff748c7751
jacadzaca <vitouejj@gmail.com>
2019-11-06 21:26:11 +0100

    added a way to interface with the game

--- chess/chess.py
+++ chess/chess.py
@@ -2,14 +2,12 @@ from board import Board
 
 
 def main():
-    chess_board = Board()
-    chess_board.execute_command('06 04')
-    chess_board.execute_command('11 13')
-    chess_board.execute_command('04 13')
-    chess_board.execute_command('01 02')
-    chess_board.execute_command('16 15')
-    chess_board.execute_command('02 01')
+    chess_board = Board(8, 8)
     print(chess_board)
+    while True:
+        command = input()
+        chess_board.execute_command(command[0], command[1])
+        print(chess_board)
 
 
 if __name__ == '__main__':
