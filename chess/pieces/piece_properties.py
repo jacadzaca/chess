44eac950176083118cfed75ffe6a7a6c273e5d4e
jacadzaca <vitouejj@gmail.com>
2020-04-05 15:54:38 +0200

    pices are now colored

--- chess/pieces/piece_properties.py
+++ chess/pieces/piece_properties.py
@@ -1,10 +1,18 @@
+_SET_COLOR_TERMIANL_COMMAND = '\033['
+_RESET_COLOR_TERMINAL_COMMAND = '0m'
+
+
 class PieceProperties():
     def __init__(self,
                  owner,
                  representation,
                  is_jumper=False, is_king=False, already_moved=False):
         self.owner = owner
-        self._representation = representation
+        self._representation = (_SET_COLOR_TERMIANL_COMMAND
+                                + owner.value
+                                + representation
+                                + _SET_COLOR_TERMIANL_COMMAND
+                                + _RESET_COLOR_TERMINAL_COMMAND)
         self.is_jumper = is_jumper
         self.is_king = is_king
         self.already_moved = already_moved
--- chess/players.py
+++ chess/players.py
@@ -1,7 +1,11 @@
 from enum import Enum
 
+# https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+_WHITE_ESCAPE_CODE = '97m'
+_BLACK_ESCAPE_CODE = '90m'
+
 
 class Player(Enum):
     """enum containing possible players"""
-    WHITE = 0
-    BLACK = 1
+    WHITE = _WHITE_ESCAPE_CODE
+    BLACK = _BLACK_ESCAPE_CODE
