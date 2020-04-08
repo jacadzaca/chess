e5d30aea5713b547ce68f8ef3af04eb29a7c0a2b
jacadzaca <vitouejj@gmail.com>
2020-04-09 01:50:58 +0200

    removed unused return

--- chess/board.py
+++ chess/board.py
@@ -16,24 +16,19 @@ class Board:
 
     def execute_attack(self, command):
         '''@param see command.py
-           @returns the knocked over pice
            is_valid_attack is expected to be invoked before this method
            it's up to user to change the turn'''
-        knocked_over_piece = command.desired_tile.piece
         command.desired_tile.piece = None
         command.desired_tile.piece = command.tile.piece
         command.tile.piece = None
-        return knocked_over_piece
 
     def execute_move(self, command):
         '''@param see command.py
-           @returns the piece, which was moved
            is_valid_move is expected to be invoked before this method
            it's up to user to change the turn'''
         command.desired_tile.piece = command.piece
         command.tile.piece = None
         command.piece.already_moved = True
-        return command.piece
 
     def is_valid_move(self, command):
         return (self._is_pices_owners_turn(command)
